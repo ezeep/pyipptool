f23be7a14c8ef57037946335bdd9b3a44aaa451a
Nicolas Delaby <nicolas.delaby@ezeep.com>
2014-05-18 17:24:54 +0200

    run tests against python3.4

--- setup.py
+++ setup.py
@@ -39,8 +39,9 @@ setup(
     packages=('pyipptool',),
     install_requires=('deform>=2.0a2',),
     extra_requires={'Tornado': ('tornado', 'futures')},
-    tests_require=('mock', 'pytest', 'coverage', 'pep8',
-                   'pytest-cov', 'coveralls', 'pkipplib', 'tornado'),
+    tests_require=('mock', 'pytest', 'coverage', 'pytest-pep8',
+                   'pytest-cov', 'coveralls', 'pkipplib', 'tornado',
+                   'tox',),
     include_package_data=True,
     test_suite='tests',
     cmdclass = {'test': PyTest},
@@ -51,6 +52,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Printing',
     ]
 )
